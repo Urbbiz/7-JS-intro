37c7d86c91d795272428c35b1bc0943c2de230cb
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-11-11 16:16:19 +0200

    oplia

---
+++ js/main.js
@@ -0,0 +1,79 @@
+
+
+// spausdiname sveikaji skaiciu 
+console.log (4646);
+
+// du skaiciai versus desimtainiai - kaip eikia ir koks yra kablelis
+console.log(3,14);
+console.log(3.14);
+
+//  kaip spausdinamas tekstas
+console.log("Labas rytas");
+console.log('Labas rytas');
+
+
+
+// **********************************************
+// KINTAMIEJI
+// **********************************************
+
+// Iniciavimo budai:
+
+
+const pirmasSkaicius = 7;
+const antrasSkaicius = 3;
+const suma = pirmasSkaicius + antrasSkaicius
+console.log(suma);
+console.log(antrasSkaicius);
+
+console.log(pirmasSkaicius, '+', antrasSkaicius, '=', suma)
+
+
+
+// Rikis megstamas maistas yra kaulas.
+const suo = 'Rikis';
+const maistas = 'kaulas';
+
+
+const sakinys = suo + ' megstamas maistas  yra ' 
++ maistas + '.'
+console.log(sakinys)
+
+// Chuck Norris (80)
+const heroName = 'Chuck';
+const heroLastname = 'Norris';
+const birth = 1940;
+const year =2020
+const age = year - birth
+
+
+
+const sakinys3= heroName + ' ' + heroLastname + ' (' + age + ')'
+console.log(sakinys3);
+
+
+console.clear()
+
+let pinigine = 7;
+const sausis = 100;
+const vasaris = 200;
+const kovas = 70;
+
+// 107
+// 307
+// 377
+
+pinigine = pinigine + sausis;
+console.log(pinigine);
+
+pinigine = pinigine + vasaris;
+console.log(pinigine);
+
+pinigine = pinigine + kovas;
+console.log(pinigine);
+
+
+console.clear()
+
+
+
